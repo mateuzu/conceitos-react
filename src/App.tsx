e92d866a65ce439361f0c5725c46f2bd303e33ee
Mateus Ferreira <mateusf63@gmail.com>
2023-07-26 12:05:39 -0300

    Criando rotas utilizando React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
-import './App.css'
-// import Flexbox from './components/Flexbox/Flexbox';
-import Grid from './components/Grid/Grid';
+import './App.css';
+import Home from './paginas/home/Home';
+import Navbar from './components/navBar/NavBar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
 
-function App(){
+function App() {
   return (
-  <>
-    {/* <Flexbox/> */}
-    <Grid/>
-  </>
-  ); 
+    <>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+);
 }
-
 export default App;
+
+/*
+  TAG BrowserRouter:
+  Habilita o controle de rotas (container para os componentes da aplicação que precisam ser renderizadas com base em rotas)
+  O componente BrowserRouter é importado da biblioteca React Router DOM e é usado para envolver todo o conteúdo que precisa de roteamento.
+	
+  TAG Routes: 
+  Elemento que identifica qual foi a rota acessada pelo usuário a partir da URL do navegador e verifica se existe um componente para a rota
+  O componente Routes é importado da biblioteca React Router DOM e é usado para definir as rotas do aplicativo.
+
+  TAG Route:
+  <Route path="/" = Caminho/URL raiz da página, ou seja,, o caminho da página inicial
+  <Route path="/caminho" = Determinando caminho/URL para renderizar determinada página e exibir o elemento correpondente em tela
+  <Route path="/" element={elemento que será acessado caso a URL for acessada} />
+
+  OBS:
+  Os elementos que estão fora do escopo do Routes sempre serão renderizados independente da rota que o usuário acessar.
+
+  Vale ressaltar que os componentes devem ser inseridos conforme sua ordem de inserção , neste exemplo, o componente <Navbar> será exibido acima/antes, enquanto <Footer> será exibido abaixo/depois.
+  Como o React lê os códigos como se fosse um livro (da esquerda pra direta, de cima pra baixo) o componente será exibido com base em sua inserção, dessa forma se o componente <Footer> for inserido antes do <Navbar>, ele será exibido em tela anteriormente
+*/
--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,32 +1,19 @@
-import homeLogo from '../../assets/home.png'
-import './Home.css';
+import React from 'react';
 
-function Home() {
+
+function Login() {
     return (
         <>
-            <div className="bg-indigo-900 flex justify-center">
-                <div className='container grid grid-cols-2 text-white'>
-                    <div className="flex flex-col gap-4 items-center justify-center py-4">
-                        <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
-                        <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+            <div className="h-96 mb-96">
 
-                        <div className="flex justify-around gap-4">
+                <h1 className='text-xl'>Login</h1>
 
-                            <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
-                        </div>
-                    </div>
-
-                    <div className="flex justify-center ">
-                        <img src={homeLogo} alt="" className='w-2/3' />
-                    </div>
-                </div>
             </div>
-
         </>
     );
 }
 
-export default Home;
+export default Login;
 
 /*
     Bibliotecas de roteamento em desenvolvimento front-end
@@ -45,5 +32,5 @@ export default Home;
     Com a TAG <Link>, somente o conteúdo adicional/diferente será renderizado em tela, ao invés do conteúdo inteiro
     
     No exemplo:
-    <Link to='/login' className='hover:underline mx-4'>Login por Link</Link> - Uma forma de navegar entre páginas utilizando padrão SPA para que somente o contéudo adicional seja renderizado
+    <Link to='/home' className='hover:underline  mx-4'>Login por Link</Link> - Uma forma de navegar entre páginas utilizando padrão SPA para que somente o contéudo adicional seja renderizado
 */
